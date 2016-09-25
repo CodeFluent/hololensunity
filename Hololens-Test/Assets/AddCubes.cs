7545b98353a40625eac1fa431f929f7d873e9766
Wasfi Momen <wasfimomenwim@gmail.com>
2016-09-24 21:55:22 -0400

    Added CS script to get info

---
+++ Hololens-Test/Assets/AddCubes.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    public string url = "https://jsonplaceholder.typicode.com/posts";
+
+    IEnumerator Start()
+    {
+        WWW www = new WWW(url);
+        yield return www;
+
+        if (www.text == null)
+        {
+            Debug.Log("WWW Ok!: " + www.text);
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+
+    }
+}
